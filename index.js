c3c5ea57e73c16ed34d7dafc20285a4d6cc3b684
Peter Aronov <PeterAronov2@gmail.com>
2022-07-19 14:09:52 +0300

    added dotenv package, 7/8 comments resolved

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const messagesRoute = require('./api/messages/messages'); //access to to this file 
 
@@ -11,6 +12,6 @@ app.use(bodyParser.json());
 
 app.use("/messages", messagesRoute);//every request for /messages rout will go to ./routes/messages-routes
 
-app.listen(port, '127.0.0.1', () => {
-  console.log(`application start at ${port}`);
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log("application start at", process.env.PORT);
 });
