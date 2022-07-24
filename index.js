38ce4de0f810047854ede7eff4c1573ed9914e3f
Peter Aronov <PeterAronov2@gmail.com>
2022-07-24 10:04:51 +0300

    comments before PR

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-require('dotenv').config();
+const app = require("./app");
+require('dotenv').config({ path: './config/index.env' }); // Default: path.resolve(process.cwd(), '.env')
 
-const messagesRoute = require('./api/messages/messages'); //access to to this file 
+const port = process.env.PORT;
+const ip = process.env.IP;
 
-const app = express();
-const port = 8000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use("/messages", messagesRoute);//every request for /messages rout will go to ./routes/messages-routes
-
-app.listen(process.env.PORT, process.env.IP, () => {
-  console.log("application start at", process.env.PORT);
+app.listen(port, ip, () => {
+  console.log("Application starts at", port, "at IP:", ip);
 });
