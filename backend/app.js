818143c36db78d7a7f1890facf88744100bb7af8
Peter Aronov <PeterAronov2@gmail.com>
2022-08-03 03:36:29 +0300

    Use path library inside static method - fixed

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,12 @@ const path = require("path");
 const messagesRoute = require('./api/v1/messages.router'); // Access to to this file 
 
 const app = express();
+const forntedFolderPath = path.join(__dirname, '../fronted');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../fronted')))
+app.use(express.static(forntedFolderPath))
 app.use("/messages", messagesRoute);// Every request for /messages rout will go to ./routes/messages-routes
 
 module.exports = app;
