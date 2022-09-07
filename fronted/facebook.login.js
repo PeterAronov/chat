8670c4e37c9fc7d205e74873dfbb9d43eb2b8af8
Peter Aronov <PeterAronov2@gmail.com>
2022-09-07 15:43:35 +0300

    Saving user name in local storage

--- fronted/facebook.login.js
+++ fronted/facebook.login.js
@@ -1,12 +1,12 @@
 statusChangeCallback = (response) => {  // Called with the results from FB.getLoginStatus().
     console.log('statusChangeCallback');
     console.log(response);                   // The current login status of the person.
-    // if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-    //     testAPI();
-    // } else {                                 // Not logged into your webpage or we are unable to tell.
-    //     document.getElementById('status').innerHTML = 'Please log ' +
-    //         'into this webpage.';
-    // }
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
 }
 
 
@@ -30,6 +30,11 @@ window.fbAsyncInit = function () {
     });
 };
 
-Peter = () => {
-    console.log("Peter")
+testAPI() = () => {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      localStorage.setItem("myUserName", response.name);
+      document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!'
+    })
 }
