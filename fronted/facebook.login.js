530a943c940ba476b54bd9447657e0e65be54f1f
Peter Aronov <PeterAronov2@gmail.com>
2022-09-07 15:47:32 +0300

    removed old login

--- fronted/facebook.login.js
+++ fronted/facebook.login.js
@@ -1,3 +1,12 @@
+testAPI() = () => {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      localStorage.setItem("myUserName", response.name);
+      document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!'
+    })
+}
+
 statusChangeCallback = (response) => {  // Called with the results from FB.getLoginStatus().
     console.log('statusChangeCallback');
     console.log(response);                   // The current login status of the person.
@@ -28,13 +37,4 @@ window.fbAsyncInit = function () {
     FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
         statusChangeCallback(response);        // Returns the login status.
     });
-};
-
-testAPI() = () => {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      localStorage.setItem("myUserName", response.name);
-      document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!'
-    })
 }
