c873191d1bd4f54f038211ef2d941ac2057c74c3
Peter Aronov <PeterAronov2@gmail.com>
2022-09-14 08:29:53 +0300

    Refactoring of facebook login 8

--- fronted/services/dom.service.js
+++ fronted/services/dom.service.js
@@ -1,6 +1,7 @@
 initChatMessagesAfterLogin = () => {
     document.getElementsByClassName('message-container')[0].style.display = "block"; // getElementsByClassName returns an array of elements with the same class name
                                                                                      // "block" means that the element is displayed as a block element (like <p>) and not hidden                                                                
+    const userName = getLocalStorageUserName();
     const messageTextDiv = document.getElementById("new-message");
     messageTextDiv.placeholder = userName + " Say something . . ."
 
