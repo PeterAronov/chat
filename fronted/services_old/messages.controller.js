15e758321d0b4f3f6b186745cd41bce3f2ccc8b0
Peter Aronov <PeterAronov2@gmail.com>
2022-10-26 13:17:32 +0300

    using template literals 4

--- fronted/services_old/messages.controller.js
+++ fronted/services_old/messages.controller.js
@@ -45,7 +45,7 @@ const displayAllMessages = (messagesObjectArray) => {
         <div class="message-container">
         <div class="message-time">${new Date(message.createdAt).toLocaleString()}</div>
         <div class="message-body">${message.name}: ${message.text}
-            <span class="deleteMessage" onclick="deleteMessage(${message._id})">×</span>
+            <span class="deleteMessage" onclick='deleteMessage(${message._id})'>×</span>
         </div>
     </div>
     `
