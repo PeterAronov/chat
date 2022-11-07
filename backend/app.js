95ce78a198fb7d22e65dc259fd24853aa425dc74
Peter Aronov <PeterAronov2@gmail.com>
2022-11-07 05:08:34 +0200

    auth via passport 15

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ const app = express();
 const forntedFolderPath = path.join(__dirname, '../fronted');
 const viewsFolderPath = path.join(__dirname, '../fronted/views');
 
-app.set('view engine', 'ejs');
-app.set('views', viewsFolderPath); // Setting the view doesn't mean index.html will be rendered, it means that the view engine will look for the index.ejs file in the views folder.
+// app.set('view engine', 'ejs');
+// app.set('views', viewsFolderPath); // Setting the view doesn't mean index.html will be rendered, it means that the view engine will look for the index.ejs file in the views folder.
 
 app.use(session({ // when a request ends there is not communication between the server and the client, we can save data inside req.session object and it will be available in the next request.
     resave: false, // means that we don't want to save the session if nothing is modified
@@ -30,12 +30,12 @@ app.use(bodyParser.json());
 
 app.use(express.static(forntedFolderPath))
 
-app.get('/', passport.authenticate('facebook'), (req, res) => {
-    // app.get('/',(req, res) => { 
+// app.get('/', passport.authenticate('facebook'), (req, res) => {
+//     // app.get('/',(req, res) => { 
 
-    console.log('home.ejs');
-    res.render('home', { name: 'Programmer' });
-})
+//     console.log('home.ejs');
+//     res.render('home', { name: 'Programmer' });
+// })
 
 app.use(authRoute)
 app.use(messagesRoute)// Every request for /messages rout will go to ./routes/messages-routes
